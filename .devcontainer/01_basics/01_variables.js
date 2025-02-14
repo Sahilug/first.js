7174d0373d94d6107fb0f93403f9e617fba6ad8a
Sahil Ghadge <111067960+Sahilug@users.noreply.github.com>
2025-02-14 08:55:56 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=37484
+let accountEmail="sahilghadge1802@gmail.com"
+var accoutpassword="8999"
+/*
+prefer not to use var because of its issues in block and scope,functional scope
+*/
+accountcity="Kashmir"
+accountEmail="sostsahil@gmail.com"
+accoutpassword="568344"
+accountcity="Hydrabad"
+console.log(accountId);
+console.table([accountId,accountEmail,accoutpassword,accountcity]);
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as newer version
+//alert(3+3)//we are using nodejs,not browser
+console.log("sahil")
+
+let name="poppye"
+let age=2
+let isloggedIn=true
+
+
+//number
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol=for uniqness
+
+//object
+console.log(typeof null)
+console.log(typeof undefined)
