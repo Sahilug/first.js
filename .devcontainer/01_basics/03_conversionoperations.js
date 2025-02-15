81e3c9d22cc201309a5e47df5adf40e4c90cf62a
Sahil Ghadge <111067960+Sahilug@users.noreply.github.com>
2025-02-15 14:11:55 +0000

    Conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionoperations.js
@@ -0,0 +1,18 @@
+let score="sahil"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33"=33
+//"33abc"=NaN
+//true=1; false=0,(" "=false),("sahil"=true)
+
+let NNumber=33
+let string=String(NNumber)
+console.log(string);
+console.log(typeof string);
+
